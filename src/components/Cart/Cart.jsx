f61f76bf5c34a11b37c7008119ee8c3ba8c40b7e
sabbirsiddique <sabbirsiddique6798@gmail.com>
2023-09-15 03:47:44 +0600

    display selected course, muitiple course selection avoided and totai credit and remaining funtionality ok

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,23 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
 
-const Cart = () => {
-    return (
-        <div>
-             <h1>credit remianing</h1>
-        </div>
-    );
+const Cart = ({ selectedCourse, totalCreditHour, creditRemaining }) => {
+  return (
+    <div className="card w-[312px] h-[402px] bg-base-100 shadow-xl rounded-xl sticky top-0">
+      <h1 className="text-center">Credit Hour Remainings: {creditRemaining} hr</h1>
+      <hr />
+      <h1>
+        Course Name
+        {selectedCourse.map((course) => (
+          <li key={course.id}>{course.title}</li>
+        ))}
+      </h1>
+      <hr />
+      <h1>Total Credit Hour: {totalCreditHour} hr</h1>
+    </div>
+  );
 };
 
 export default Cart;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,6 +7,9 @@ import { BsBook } from "react-icons/bs";
 
 const Home = () => {
   const [allCourses, setAllCourses] = useState([]);
+  const [selectedCourse, setSelectedCourse] = useState([]);
+  const [totalCreditHour, setTotalCreditHour] = useState(0);
+  const [creditRemaining, setCreditRemaining] = useState(20);
 
   useEffect(() => {
     fetch("./data.json")
@@ -14,43 +17,72 @@ const Home = () => {
       .then((data) => setAllCourses(data));
   }, []);
 
+  const handleSelectCourse = (course) => {
+    const isExist = selectedCourse.find((courses) => courses.id == course.id);
+
+    let credit = course.credit;
+
+    if (isExist) {
+      return alert("Already selected");
+    } else {
+      selectedCourse.forEach((courses) => {
+        credit = credit + courses.credit;
+      });
+      const remaining = 20 - credit;
+
+      if (credit > 20) {
+        return alert("No more credit remaining");
+      } else {
+        setTotalCreditHour(credit);
+        setCreditRemaining(remaining);
+
+        setSelectedCourse([...selectedCourse, course]);
+      }
+    }
+  };
+
   return (
     <div className="max-w-screen-2xl mx-auto flex">
       <div className="flex flex-wrap gap-8 w-2/3 mx-auto">
-        {
-            allCourses.map((course)=>(
-                <div key={course.id} className="card w-[312px] h-[402px] bg-base-100 shadow-xl rounded-xl">
-          <figure className="px-4 pt-4">
-            <img
-              src={course.img}
-              className="rounded-xl w-full"
-            />
-          </figure>
-          <div className="px-4">
-            <p className="mt-4">{course.title}</p>
-            <p className="mt-4">
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout.
-            </p>
+        {allCourses.map((course) => (
+          <div
+            key={course.id}
+            className="card w-[312px] h-[402px] bg-base-100 shadow-xl rounded-xl"
+          >
+            <figure className="px-4 pt-4">
+              <img src={course.img} className="rounded-xl w-full" />
+            </figure>
+            <div className="px-4">
+              <p className="mt-4">{course.title}</p>
+              <p className="mt-4">
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+              </p>
 
-            <div className="flex justify-around mt-3 items-center">
-              <FiDollarSign></FiDollarSign>
-              <p>Price: {course.price}</p>
-              <BsBook></BsBook>
-              <p>Credit: {course.credit}</p>
+              <div className="flex justify-around mt-3 items-center">
+                <FiDollarSign></FiDollarSign>
+                <p>Price: {course.price}</p>
+                <BsBook></BsBook>
+                <p>Credit: {course.credit}hr</p>
+              </div>
+
+              <button
+                onClick={() => handleSelectCourse(course)}
+                className="bg-[#2F80ED] w-full mt-3 text-white rounded-lg p-1"
+              >
+                Select
+              </button>
             </div>
-
-            <button className="bg-[#2F80ED] w-full mt-3 text-white rounded-lg p-1">
-              Select
-            </button>
           </div>
-        </div>
-            ))
-        }
+        ))}
       </div>
 
       <div className="w-1/3 mx-auto">
-        <Cart></Cart>
+        <Cart
+          selectedCourse={selectedCourse}
+          totalCreditHour={totalCreditHour}
+          creditRemaining={creditRemaining}
+        ></Cart>
       </div>
     </div>
   );
