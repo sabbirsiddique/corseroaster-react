6b101b2c7afc1c7f228a1d5e9c92c8f2fed0f7aa
sabbirsiddique <sabbirsiddique6798@gmail.com>
2023-09-14 21:06:31 +0600

    Environment setup for project is completed and components created

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div>
+             <h1 className="text-3xl font-bold">hi!!!!!!</h1>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Cart from "../Cart/Cart";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <Cart></Cart>
+    </div>
+  );
+};
+
+export default Home;
